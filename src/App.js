938d66a79e6ee0389209e397e38138354d716673
Bruno Monarini <bruno.monarini@perugluglu.net>
2022-06-14 00:55:08 -0300

    Ajuste no css

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ import Tela06 from "./components/tela06/Tela06";
 //   // }
 function App() {
   const [tela02, setTela2] = useState("grid")
-  const [tela03, setTela3] = useState("grid")
+  const [tela03, setTela3] = useState("grid overflow-y")
   const [tela04, setTela4] = useState("grid")
   const [tela05, setTela5] = useState("grid")
   const [tela06, setTela6] = useState("grid")
@@ -26,7 +26,7 @@ function App() {
     <div className="App">
       <Tela01 tela02={tela02} setTela2={setTela2} />
       <Tela02 tela02={tela02} tela03={tela03} setTela3={setTela3}/>
-      {tela03 === "grid" ? (<Tela03 tela03={tela03} tela04={tela04} setTela4={setTela4} />) : null}
+      <Tela03 tela03={tela03} tela04={tela04} setTela4={setTela4} />
       <Tela04 tela04={tela04} tela05={tela05} setTela5={setTela5} />
       <Tela05 tela05={tela05} tela06={tela06} setTela6={setTela6}/>
       <Tela06 tela06={tela06}/>
